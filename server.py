a100c18d2dcfb14782337a77175f22d7a1d0cbf5
Claret Nnamocha <devclareo@gmail.com>
2020-03-17 11:31:19 +0100

    Update server.py

--- server.py
+++ server.py
@@ -4,5 +4,5 @@ import os
 LOCAL_PORT = 6060
 
 port = int(os.environ.get("PORT", LOCAL_PORT))
-host = "localhost" if port == LOCAL_PORT else ""
+host = "localhost" if os.environ.get("PORT") == None else ""
 serv.run(host=host, port=port)
